2c03063281c3ed76d08ff194da635892ee4c91e5
Sergey Senozhatsky <senozhatsky@chromium.org>
2025-03-03 11:03:27 +0900

    UPSTREAM: zram: do not leak page on writeback_store error path
    Ensure the page used for local object data is freed on error out path.
    
    Link: https://lkml.kernel.org/r/20250303022425.285971-19-senozhatsky@chromium.org
    Fixes: 330edc2bc059 (zram: rework writeback target selection strategy)
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: Kairui Song <ryncsn@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Yosry Ahmed <yosry.ahmed@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit a6d2193b3ef53d5c5a2c56d397227f5e891f2df3)
    
    Bug: 446676432
    Change-Id: I19d2a2d4115b5a94268f4b030e4face6b9fc2d7d
    Signed-off-by: David Stevens <stevensd@google.com>
    (cherry picked from commit d3f6b5baf6d091e3ad9318297e5553865173ca92)
    (cherry picked from commit 4b66b6fb67160ca872cea7684c1d237bcd1709fb)

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1859,7 +1859,7 @@ static ssize_t writeback_store(struct device *dev,
 	unsigned long index = 0;
 	struct bio bio;
 	struct bio_vec bio_vec;
-	struct page *page;
+	struct page *page = NULL;
 	ssize_t ret = len;
 	int mode, err;
 	unsigned long blk_idx = 0;
@@ -2030,8 +2030,10 @@ next:
 #else
 		free_block_bdev(zram, blk_idx);
 #endif
-	__free_page(page);
+
 release_init_lock:
+	if (page)
+		__free_page(page);
 	up_read(&zram->init_lock);
 
 	return ret;
