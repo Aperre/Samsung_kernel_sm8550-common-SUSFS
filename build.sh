8e53532b089a38e9ab15d6e637c248acedd35e9f
Aperre <60411288+Aperre@users.noreply.github.com>
2026-02-17 02:50:07 +0000

    build.sh: discovered that manual hook shouldnt be enabled with susfs

--- build.sh
+++ build.sh
@@ -95,10 +95,10 @@ KSUN_CHECKOUT_HASH=""    # Specific KernelSU-Next commit SHA
 
 ## SUKISU-Ultra Options
 ENABLE_SUKISU=1          # Use SUKISU-Ultra? (1 = yes, 0 = no)
-SUKI_MANUAL_HOOKS=1      # Manual Hooks for SUKISU (SUSFS version only) (1 = manual, 0 = default)
+SUKI_MANUAL_HOOKS=0      # Manual Hooks for SUKISU (SUSFS version only) (1 = manual, 0 = default)
 SUKI_TRACEPOINTS_HOOK=0  # Use tracepoint hook for Sukisu-Ultra (for SUSFS and Normal ver) (1 = enable, 0 = disabled)
 SUKI_CHECKOUT_HASH=""    # Specific SUKISU commit SHA
-PATCH_KPM=0              # Patches the kernel binary after its done compiling.
+PATCH_KPM=1              # Patches the kernel binary after its done compiling.
 KPM_VERSION="0.13.0"     # Release tag of KPM binary
 
 ## KernelSU Options     | Note KernelSU-Next removed SUSFS support from their branch
