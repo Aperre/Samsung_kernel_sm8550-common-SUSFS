40567f8a33dcdd35bd22db5e8c21c79d6a9844ac
Aperre <ape@aqpe.dev>
2026-06-06 21:58:40 +0100

    ANDROID: cleancache: Fix duplicate rbin_module_oem_func typedef

--- include/linux/cleancache.h
+++ include/linux/cleancache.h
@@ -41,8 +41,6 @@ struct cleancache_ops {
 };
 typedef int (*rbin_module_oem_func)(int, int *);
 
-typedef int (*rbin_module_oem_func)(int, int *);
-
 extern int cleancache_register_ops(const struct cleancache_ops *ops);
 extern void __cleancache_init_fs(struct super_block *);
 extern void __cleancache_init_shared_fs(struct super_block *);
